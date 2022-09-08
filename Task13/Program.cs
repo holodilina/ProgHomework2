963d809fc8634e4912136c5bbd522f78a53e9180
Natalya <holodilina.n@gmail.com>
2022-09-08 16:54:26 +0300

    Add folder Task13

---
+++ Task13/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+
+while(true)
+{
+    string input = Console.ReadLine();
+    if(!input.Equals("exit"))
+       Console.WriteLine("Третья цифра числа {0} -> {1}", input, input[2]);    
+
+    else
+
+       break;
+}
