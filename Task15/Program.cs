8db0399c683b35c8c543b03dd38bc55bd9a794b6
Natalya <holodilina.n@gmail.com>
2022-09-09 14:52:12 +0300

    добавление названия к заданию 15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,4 +1,4 @@
-﻿// 
+﻿// Программа, которая выводит будний или выходной день недели.
 
 Console.Write("Введите число: ");
 
@@ -9,15 +9,15 @@ while(true)
     if(!string.IsNullOrWhiteSpace(input)&&input.Equals("exit"))
       break;
 
-      if(string.IsNullOrWhiteSpace(input)
-      ||!int.TryParse(input, out dayOfWeek)
-      ||dayOfWeek<1
-      ||dayOfWeek>7)
-      {
-         Console.WriteLine("Введите день недели от 1 до 7");
-         continue;
-      }
- 
-      Console.WriteLine(dayOfWeek==6||dayOfWeek==7?"Выходной":"Будний"); 
+    if(string.IsNullOrWhiteSpace(input)
+    ||!int.TryParse(input, out dayOfWeek)
+    ||dayOfWeek<1
+    ||dayOfWeek>7)
+    {
+        Console.WriteLine("Введите день недели от 1 до 7");
+        continue;
+    }
+
+    Console.WriteLine(dayOfWeek==6||dayOfWeek==7?"Выходной":"Будний"); 
  
 }
