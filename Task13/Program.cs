38058551a9670a4c7ddcf1ad6dd3ce80ae4f259d
Natalya <holodilina.n@gmail.com>
2022-09-09 14:45:22 +0300

    задача 15

--- Task13/Program.cs
+++ Task13/Program.cs
@@ -4,19 +4,19 @@ Console.Write("Введите число: ");
 
 while(true)
 {
-    string input = Console.ReadLine(); 
-    if(string.IsNullOrWhiteSpace(input)||!input.Equals("exit"))
+    string? input = Console.ReadLine(); 
+    int r;
+    if(!string.IsNullOrWhiteSpace(input)&&input.Equals("exit"))
+      break;
 
-      if(string.IsNullOrWhiteSpace(input)||input.Length<3)
+      if(string.IsNullOrWhiteSpace(input)
+      ||input.Length<3
+      ||!int.TryParse(input, out r))
       {
          Console.WriteLine(" -> нет третьей цифры");
       }
       else 
       {
        Console.WriteLine($"Третья цифра числа {input} -> {input[2]}"); 
-      }   
-
-    else
-
-       break;
+      }
 }
--- Task15/Program.cs
+++ Task15/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 
+
+Console.Write("Введите число: ");
+
+while(true)
+{
+    string? input = Console.ReadLine(); 
+    int dayOfWeek;
+    if(!string.IsNullOrWhiteSpace(input)&&input.Equals("exit"))
+      break;
+
+      if(string.IsNullOrWhiteSpace(input)
+      ||!int.TryParse(input, out dayOfWeek)
+      ||dayOfWeek<1
+      ||dayOfWeek>7)
+      {
+         Console.WriteLine("Введите день недели от 1 до 7");
+         continue;
+      }
+ 
+      Console.WriteLine(dayOfWeek==6||dayOfWeek==7?"Выходной":"Будний"); 
+ 
+}
